34f16d5c13763aa8ad9b91aacf15381ec7528613
Sergey <serzh251@mail.ru>
2025-02-01 17:52:20 +0300

    tile layers

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,56 +1,88 @@
-import React, {useState} from "react";
-import {MapContainer, TileLayer} from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { LayersControl, MapContainer, TileLayer, ZoomControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import '../../static/css/MapMain.css';
-// import {useAuth} from "../../hook/use-auth";
-// import Basemap from "./MapTypeChoose";
-// import {LastLocationLayer} from "./LastLocationLayer";
-
-// L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
 const position = [
-  [64, 41],
-  [62, 42],
+  [65, 42],
+  [61, 40],
 ];
 
-const MapComponent = () => {
-  const [basemap, setBasemap] = useState("osm");
-  // const {isAuth} = useAuth();
-  const basemapsDict = {
-    osm: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    sat: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-    hot: "https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png",
-    topomap: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-    dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
-    cycle: "https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
-  };
-  // const onBMChange = (bm) => {
-  //   setBasemap(bm);
-  // };
+const layers = [
+  {
+    name: 'OpenStreetMap',
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution: '&copy; OpenStreetMap contributors'
+  },
+  {
+    name: 'Google Satellite',
+    url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+    attribution: '&copy; Google Maps'
+  },
+  {
+    name: 'CartoDB Dark',
+    url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    attribution: '&copy; CartoDB'
+  },
+  {
+    name: 'ArcGIS Satellite',
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attribution: '&copy; Esri'
+  },
+  {
+    name: 'HOT',
+    url: 'https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png',
+    attribution: '&copy; Humanitarian OpenStreetMap Team'
+  },
+  {
+    name: 'OpenTopoMap',
+    url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+    attribution: '&copy; OpenTopoMap contributors'
+  },
+  {
+    name: 'Cycle Map',
+    url: 'https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+    attribution: '&copy; OpenStreetMap France'
+  }
+];
+
+
+export default function MapComponent() {
+  const [selectedLayer, setSelectedLayer] = useState(() => {
+    return localStorage.getItem('selectedLayer') || layers[0].name;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('selectedLayer', selectedLayer);
+  }, [selectedLayer]);
 
   return (
     <div className="app-layout">
-      <MapContainer bounds={position} zoom={13} style={styles.map}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a>'
-          url={basemapsDict[basemap]}
-        />
-        {/*<Basemap basemap={basemap} onChange={onBMChange}/>*/}
-        {/*{isAuth && (*/}
-        {/*  <>*/}
-        {/*    <TrackMain/>*/}
-        {/*    <LastLocationLayer/>*/}
-        {/*  </>*/}
-        {/*// )}*/}
-      </MapContainer>
+    <MapContainer
+      zoomControl={false}
+      bounds={position}
+      zoom={13}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <ZoomControl position="bottomright" />
+      <LayersControl position="topright">
+        {layers.map((layer) => (
+          <LayersControl.BaseLayer
+            key={layer.name}
+            name={layer.name}
+            checked={layer.name === selectedLayer}
+          >
+            <TileLayer
+              url={layer.url}
+              attribution={layer.attribution}
+              eventHandlers={{
+                add: () => setSelectedLayer(layer.name)
+              }}
+            />
+          </LayersControl.BaseLayer>
+        ))}
+      </LayersControl>
+    </MapContainer>
     </div>
   );
-};
-export default MapComponent;
-
-const styles = {
-  map: {
-    height: '100%',
-    width: '100%',
-  },
-};
+}
